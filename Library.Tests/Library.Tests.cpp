63845df2bb76411272a1ed5bcca4c91d7c513bce
ludmilanv <ludmilanv@mail.ru>
2022-10-10 15:38:40 +0300

    Начало работы. Пустое решение с тремя проектами Демо, Библиотека и Тесты.

---
+++ Library.Tests/Library.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LibraryTests
+{
+	TEST_CLASS(LibraryTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
